86ce6625f35fb4f7f430a9fa390b92003383cb44
Gabriel Carmo <gabriellcarmo.dev@gmail.com>
2026-06-03 10:39:37 -0300

    dia 3 - logica com Python

---
+++ dia3.py
@@ -0,0 +1,13 @@
+idade = 14
+if idade >= 18:
+    print("Maior de idade")
+else:
+    print("Menor de idade")
+
+for i in range(5):
+    print(f"Número: {i}")
+
+contador = 0
+while contador < 3:
+    print(contador)
+    contador += 1
